3acc985cf3160a842533238baad48d4efd2d5f8c
haninafisazka <112494456+haninafisazka@users.noreply.github.com>
2022-09-01 14:09:04 +0700

    Delete Hello world.java

--- Hello world.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-    // main method
-    public static void main(String[] args) {
-    // menampilkan teks ke layar
-    System.out.println("halo, ini source code OOP Java buatanku");
-    }
-}
